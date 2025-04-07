4d4909409bbacbeb34879fadb97821508b8dc679
aysel-kh <khankishiyevaysel@gmail.com>
2025-04-07 10:33:43 +0200

    исправлена ошибка в auth.py

--- auth_service/routers/auth.py
+++ auth_service/routers/auth.py
@@ -1,21 +1,39 @@
-from pydantic import BaseModel, EmailStr, ConfigDict
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import timedelta
 
 
-#Users
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: str
-    username: str
+from auth_service.database import get_db
+from auth_service import models
+from auth_service.utils.security import authenticate_user, create_access_token, get_current_user
 
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
+router = APIRouter()
 
-class UserResponse(BaseModel):
-    id: int
-    email: str
-    username: str
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # можно также вынести в настройки
 
-    class Config:
-        from_attributes = True
 
+@router.post("/token")
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        db: AsyncSession = Depends(get_db)
+):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(401, "Неверные логин или пароль")
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/verify")
+async def verify_token(
+        user: models.User = Depends(get_current_user)
+):
+     return {"username": user.username}
